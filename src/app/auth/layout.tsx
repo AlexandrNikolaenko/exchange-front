5a7b0120d49952900e138a6f7fd721ff83f6f4f5
Александр <nikol.alex06@mail.ru>
2025-09-29 23:54:34 +0300

    feat: add project structure and dependencies

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default function AuhtLayout({children} : {children: ReactNode}) {
+  return (
+    <>{children}</>
+  )
+}
---
+++ src/components/core/links/show-all.tsx
@@ -0,0 +1,3 @@
+export default function ShowAllLink() {
+  return <></>
+}
